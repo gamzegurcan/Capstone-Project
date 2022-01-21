5910ee1c2e753c1a8cfad63790b8efd73d2b6ad8
Gamze Gürcan <gmzgrcn18812@icloud.com>
2022-01-21 14:54:04 +0300

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const API_KEY = "9ae3d76bbb563cc1912fdc33de7bafd3";
+const BASE_AXIOS = axios.create({baseURL: "https://api.themoviedb.org/3"})
+
+export const fetchMovies = () => BASE_AXIOS.get(`/discover/movie?api_key=${API_KEY}&page=1`)
+
